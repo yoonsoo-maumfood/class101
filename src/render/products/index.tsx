a39309be63ab43f5fa8f58e90243638a6ff61dec
HaYoonsoo <yoonsoo@maumfood.co.kr>
2022-06-10 18:08:39 +0900

    feat: products pagination, sort, error handler 추가
    products에 pagination을 구현하였고, score를 기반으로 자동으로 sort가 되도록 했다.
    error 상태를 하나 더 추가해서, 로드 실패 여부를 확인할 수 있도록 함

--- src/render/products/index.tsx
+++ src/render/products/index.tsx
@@ -5,26 +5,48 @@ import Layout from "../../components/GlobalLayout";
 import { PageWrapper } from "./styles";
 
 const RenderProducts = () => {
+  const PRODUCTS_PER_PAGE = 5;
+  const [error, setError] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(1);
+  const [pages, setPages] = useState<number[]>([]);
   const [products, setProducts] = useState<ProductItem[]>([]);
+  const [productsToShow, setProductsToShow] = useState<ProductItem[]>([]);
 
   useEffect(() => {
     try {
-      const productsRaw = fakeFetch(Url.Products);
+      let productsRaw = fakeFetch(Url.Products);
       if (productsRaw === undefined) {
         throw new Error("product load failed");
-      } else {
-        setProducts(productsRaw);
       }
+      productsRaw.sort( (a: ProductItem, b: ProductItem) => b.score - a.score );
+      setPages(
+        Array( Math.ceil(productsRaw.length / PRODUCTS_PER_PAGE) ).fill(1).map( ( x, y ) => x + y )
+        //페이지가 3개있어야 한다면, [1, 2, 3]이런 array 형성
+      )      
+      setProducts(productsRaw);
     } catch (e) {
       console.log(e);
+      setError(true);
     }
   }, []);
+
+  useEffect( () => {
+    const pts: ProductItem[] = [];
+    for ( let index = (page-1)*PRODUCTS_PER_PAGE; index < products.length && index < page*PRODUCTS_PER_PAGE; index++ ) {
+      //보여줘야할 페이지의 인덱스 계산
+      pts.push(products[index]);
+    }
+    setProductsToShow(pts);
+  }, [page, products]);
 
   return (
     <Layout>
       <PageWrapper>
-        { products.map ( (product) => (
-          <div key={product.id}>{product.title}</div>
+        { !error && productsToShow.map ( (product) => (
+          <div key={product.id}>{product.title}{product.score}</div>
+        ))}
+        { !error && pages.map( (page) => (
+          <span key={page} onClick={ () => setPage(page) }>{page}</span>
         ))}
       </PageWrapper>
     </Layout>
