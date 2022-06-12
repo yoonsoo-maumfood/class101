7b08ae5da4b10a8b4263314f54aa91f771184202
HaYoonsoo <yoonsoo@maumfood.co.kr>
2022-06-13 01:04:08 +0900

    feat: cart 내부에서 구매할 제품 선택하고 쿠폰 적용해서 계산하는 것 구현
    productsInBuy는 productsInCart와 유사하게 작동한다. cart페이지 내부에서 구매한 제품 계산을 위해 사용한다.
    totalCost는 실시간으로 쿠폰 적용등을 계산한 결과값을 보여준다.
    코드가 조금 산만한 감이 있어서 UI 완성후에 손봐야겠다. 예컨대, productList에서는 id를 사용하고 그외 내부 state에서는 오브젝트 자체를 사용하다보니 조금 헷갈리는것이 있다.
    ProductCard에서의 cartView는 cart화면에서만 보이는 것들(구매할지 말지)을 표시할지 말지를 나타내는 변수로 사용했다

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,22 +1,39 @@
 import { ProductItem } from "../../modules/fakeFetch";
+import { useState } from "react";
 
 const ProductCard = ({
   product,
   inCart,
   addToCart,
   removeFromCart,
+  cartView,
+  inBuy,
+  onChange,
 }: {
   product: ProductItem;
   inCart: boolean;
-  addToCart: any,
-  removeFromCart: any,
+  addToCart: any;
+  removeFromCart: any;
+  cartView?: boolean;
+  onChange?: any;
+  inBuy?: boolean;
 }) => {
   return (
     <div>
       <h2>{product.title}</h2>
       <p>{product.price}</p>
       <p>{product.score}</p>
-      { inCart ? <div onClick={removeFromCart}>등록</div> : <div onClick={addToCart}>미등록</div> }
+      {!(product.availableCoupon===false) ? <div>쿠폰 사용 가능</div> : ""}
+      {inCart ? (
+        <div onClick={removeFromCart}>등록</div>
+      ) : (
+        <div onClick={addToCart}>미등록</div>
+      )}
+      {cartView ? (
+        <input type="checkbox" checked={inBuy} onChange={onChange} />
+      ) : (
+        ""
+      )}
     </div>
   );
 };
--- src/render/cart/index.tsx
+++ src/render/cart/index.tsx
@@ -30,7 +30,24 @@ const RenderCart = () => {
   } = useCart();
 
   //coupon related values
+  const [coupon, setCoupon] = useState<Coupon | null>(null);
   const [coupons, setCoupons] = useState<Coupon[]>([]);
+
+  //buy related values
+  const [productsInBuy, setProductsInBuy] = useState<ProductItem[]>([]);
+  const removeFromBuy = (product: ProductItem) => {
+    setProductsInBuy(productsInBuy.filter((p) => p.id !== product.id));
+  };
+  const toggleBuy = (product: ProductItem) => {
+    const index = productsInBuy.findIndex((p) => p.id === product.id);
+    if (index === -1) {
+      setProductsInBuy([...productsInBuy, product]);
+    } else {
+      setProductsInBuy(productsInBuy.filter((p, i) => i !== index));
+    }
+  };
+
+  const [totalCost, setTotalCost] = useState<number>(0);
 
   useEffect(() => {
     //when products change
@@ -75,21 +92,69 @@ const RenderCart = () => {
     setProductsToShow(pts);
   }, [page, productsInCart]);
 
+  useEffect(() => {
+    let sum = 0;
+
+    if (coupon === null) {
+      for (const product of productsInBuy) {
+        sum += product.price;
+      }
+    } else {
+      if (coupon.type === "rate") {
+        for (const product of productsInBuy) {
+          if (!(product.availableCoupon === false)) {
+            sum += product.price * (100 - (coupon.discountRate as number)) * 0.01;
+          } else {
+          sum += product.price;
+          }
+        }
+      } else if (coupon.type === "amount") {
+        for (const product of productsInBuy) {
+          sum += product.price;
+        }
+        sum -= coupon.discountAmount as number;
+        if (sum < 0) {
+          sum = 0;
+        }
+      } else {
+        throw new Error("invalid coupon");
+      }
+    }
+
+    setTotalCost(sum);
+  }, [coupon, productsInBuy]);
+
   return (
     <Layout>
       <PageWrapper>
         {productsToShow.map((product) => (
           <ProductCard
+            cartView={true}
             product={product}
             key={product.id}
+            inBuy={
+              !(productsInBuy.find((p) => p.id === product.id) === undefined)
+            }
             inCart={!(cartList.find((id) => id === product.id) === undefined)}
             addToCart={() => addToCart(product.id)}
-            removeFromCart={() => removeFromCart(product.id)}
+            removeFromCart={() => {
+              removeFromCart(product.id);
+              removeFromBuy(product);
+            }}
+            onChange={() => toggleBuy(product)}
           />
         ))}
         <PageNavigator pages={pages} currentPage={page} setPage={setPage} />
-        
-        <select>
+
+        <select
+          onChange={(e: any) => {
+            if (e.target.value === "") {
+              setCoupon(null);
+            } else {
+              setCoupon(coupons[e.target.value]);
+            }
+          }}
+        >
           <option value="">-</option>
           {coupons.map((coupon, index) => (
             <option key={index} value={index}>
@@ -97,6 +162,8 @@ const RenderCart = () => {
             </option>
           ))}
         </select>
+
+        <div>총합: {totalCost}</div>
       </PageWrapper>
     </Layout>
   );
